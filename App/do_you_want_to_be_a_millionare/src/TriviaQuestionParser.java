fc28aa3308354af5aecb0fadfdd7067bcd72661e
Alexander Kokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2024-03-11 15:44:38 -0400

    bug fix

--- App/do_you_want_to_be_a_millionare/src/TriviaQuestionParser.java
+++ App/do_you_want_to_be_a_millionare/src/TriviaQuestionParser.java
@@ -4,15 +4,15 @@ import org.json.JSONArray;
 
 public class TriviaQuestionParser {
 
-  public static void parseAndDisplayQuestion (String jsonResponse) { 
+  public static void parseAndDisplayQuestion(String jsonResponse) {
     JSONObject jsonObject = new JSONObject(jsonResponse);
-    JSONArray results = jsonObject.getJSONArray( key: "results"); 
-    JSONObject firstResult = results.getJSONObject(index: 0);
-    String question = firstResult.getString( key: "question");
-    String correctAnswer = firstResult.getString( key: "correct_answer"); 
-    JSONArray incorrectAnswers = firstResult.getJSONArray( key: "incorrect_answers");
-    System.out.println("Question: "+ question);
+    JSONArray results = jsonObject.getJSONArray("results");
+    JSONObject firstResult = results.getJSONObject(0);
+    String question = firstResult.getString("question");
+    String correctAnswer = firstResult.getString("correct_answer");
+    JSONArray incorrectAnswers = firstResult.getJSONArray("incorrect_answers");
+    System.out.println("Question: " + question);
     System.out.println("Correct Answer: " + correctAnswer);
-    System.out.println("Incorrect Answers: " +  incorrectAnswers.join( separator: ", "));
+    System.out.println("Incorrect Answers: " + incorrectAnswers.join(", "));
   }
 }
